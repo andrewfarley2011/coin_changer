ebb78a89d7dbb7cc71e09f90a2f3c97a385d7a0b
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-24 18:44:25 -0400

    coin_changer_b

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -1,32 +1,46 @@
 
-# require "minitest/autorun" 
-# require_relative "multi-dimensional_array.rb" 
-# class Multi_Dimensional_Array < Minitest::Test  
-
-#   def test_1_equals_1
-#     assert_equal(1,1)
-#   end
-
-
-# end 
-
 require "minitest/autorun"
-require_relative "random_pairing.rb"
+require_relative "coin_changer.rb"
+class Coin_Changer < Minitest::Test
 
-class TestPairingNames < Minitest::Test 
-def test_pair	
-    names = ["Katie", "Derick", "Steph", "Ty"]
-    results = pair(names)
-    assert_equal(2, results.count)
+  def test_empty
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 0}, coin_changer(0))
   end
-  def test_multiple_pairs
-    names = ["Katie", "Beard", "Steph", "Ty", "Chris", "Robert", "Marcus", "Aaron", "Derrick G.", "Andrew", "Adrian", "John", "Steven", "Tori"]
-    results = pair(names)
-    assert_equal(7, results.count)
+
+  def test_1_cent
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coin_changer(1))
+  end
+
+  def test_2_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 2}, coin_changer(2))
+  end
+
+  def test_5_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 0}, coin_changer(5))
+  end
+
+  def test_6_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 1}, coin_changer(6))
+  end
+
+  def test_10_cents
+    assert_equal({quarter: 0, dime: 1, nickel: 0, penny: 0}, coin_changer(10))
+  end
+
+  def test_15_cents
+    assert_equal({quarter: 0, dime: 1, nickel: 1, penny: 0}, coin_changer(15))
+  end
+
+  def test_16_cents
+    assert_equal({quarter: 0, dime: 1, nickel: 1, penny: 1}, coin_changer(16))
+  end
+
+  def test_fail_purpose
+  	assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coin_changer(2))
+  end
+
 end
-  def test_odd_ball
-    p names = ["Katie", "Beard", "Steph", "Ty", "Chris", "Robert", "Mark", "Aaron", "Derrick G.", "Andrew", "Adrian", "John", "Steven", "Tori", "Tyler"]
-    p results = pair(names)
-    p assert_equal(7, results.count)
-  end
-  end 
+
+
+
+
